6d3b76de05dc646ad22cd6a56d270ac16305fdef
Liam Keegan <liam@keegan.ch>
2020-11-06 10:40:28 +0100

    improve user number input handling
      - strip all chars except numeric digits, '.' and ','
      - replace ',' with '.'
      - then parse as float
    
    also display parsed result so user can see if something is wrong

--- calculate.js
+++ calculate.js
@@ -1,22 +1,40 @@
+function parseNum(s){
+    return parseFloat(s.replace(/[^\d.,]/g, '').replace(',','.'));
+}
+
+function toPercent(x){
+    return Math.round(10000.0 * x) / 100.0 + "%";
+}
+
+function toInt(x){
+    return Math.round(x);
+}
+
 function calc(){
-    var incid = 1e-5 * parseFloat(document.getElementById('incid').value.replace(',','.'));
+    var icid_parsed = parseNum(document.getElementById('incid').value);
+    var sp_parsed = parseNum(document.getElementById('spec').value);
+    var se_parsed = parseNum(document.getElementById('sens').value);
+    var incid = 1e-5 * icid_parsed;
+    var sp = 0.01 * sp_parsed;
+    var se = 0.01 * se_parsed;
     var sample = 1000;
     var cp = incid * sample;
     var cn = sample - cp;
-    var sp = 0.01 * parseFloat(document.getElementById('spec').value.replace(',','.'));
-    var se = 0.01 * parseFloat(document.getElementById('sens').value.replace(',','.'));
     var tp = se * cp;
     var fp = cn * (1.0 - sp);
     var tn = sp * cn;
     var fn = cp * (1.0 - se);
     var ppv = tp / (tp + fp);
     var npv = tn / (tn + fn);
-    document.getElementById("tp").innerHTML = Math.round(tp);
-    document.getElementById("tn").innerHTML = Math.round(tn);
-    document.getElementById("fp").innerHTML = Math.round(fp);
-    document.getElementById("fn").innerHTML = Math.round(fn);
-    document.getElementById("ppv").innerHTML = Math.round(10000.0 * ppv) / 100.0 + "%";
-    document.getElementById("npv").innerHTML = Math.round(10000.0 * npv) / 100.0 + "%";
+    document.getElementById("icid_parsed").innerHTML = icid_parsed;
+    document.getElementById("sp_parsed").innerHTML = sp_parsed;
+    document.getElementById("se_parsed").innerHTML = se_parsed;
+    document.getElementById("tp").innerHTML = toInt(tp);
+    document.getElementById("tn").innerHTML = toInt(tn);
+    document.getElementById("fp").innerHTML = toInt(fp);
+    document.getElementById("fn").innerHTML = toInt(fn);
+    document.getElementById("ppv").innerHTML = toPercent(ppv);
+    document.getElementById("npv").innerHTML = toPercent(npv);
 }
 
 $(document).ready(function(){
